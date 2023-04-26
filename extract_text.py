e0bdbbeb66184e21c4fd8cffcc9a8099f393d882
sh gdr <goudarzi.sharzad825@gmail.com>
2023-04-26 10:42:10 +0330

    add commands to extract text form english subtitle file

--- extract_text.py
+++ extract_text.py
@@ -33,3 +33,37 @@ with open('german.txt', 'w') as f:
 
 
 
+
+"""
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++   extract english text                                     +
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+"""
+
+with open('The_Dark_Knight_English.srt') as read_file:
+    content = read_file.read()   # read main file
+
+    result = ' '
+
+    while result:
+        # use regex to detect test
+        delete_text = '[0-9]([0-9]?)([0-9]?)([0-9]?)\n[0-9][0-9]\D[0-9][0-9]\D[0-9][0-9],[0-9][0-9][0-9]\s-->\s[0-9][0-9]\D[0-9][0-9]\D[0-9][0-9],[0-9][0-9][0-9]\n'
+        result = re.search(delete_text, content)
+
+        if result:
+            text = result.group(0)
+            # delete the founded text
+            new_content = content.replace(text, '')
+
+            # modify content for the next search
+            content = new_content
+            # print(text)
+
+
+
+with open('english.txt', 'w') as f:
+    f.write(content)
+
+
+
+
